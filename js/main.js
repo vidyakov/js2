d74d4ec177956f1146b310746b5c2bb42b43d665
vidyakov <sashavidyakov2000@gmail.com>
2020-01-07 20:03:06 +0300

    Добавил отображение фоток и изменил стили меню

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const products = [
-  {id: 1, title: 'Notebook', price: 20000},
-  {id: 2, title: 'Mouse', price: 1500},
-  {id: 3, title: 'Keyboard', price: 5000},
-  {id: 4, title: 'Gamepad', price: 4500},
+  {id: 1, title: 'Notebook', price: 20000, img: 'img/not_found.jpg'},
+  {id: 2, title: 'Mouse', price: 1500, img: 'img/not_found.jpg'},
+  {id: 3, title: 'Keyboard', price: 5000, img: 'img/not_found.jpg'},
+  {id: 4, title: 'Gamepad', price: 4500, img: 'img/not_found.jpg'},
 ];
 
-const renderProduct = (title, price) => `<div class="product-item">
+const renderProduct = (img, title, price) => `<div class="product-item">
+            <img src="${img}" class="product-img">
             <h3>${title}</h3>
             <p>${price}</p>
             <button class="by-btn">Добавить в корзину</button>
@@ -16,7 +17,7 @@ const renderProduct = (title, price) => `<div class="product-item">
 
 const renderProducts = list => {
   const productList = list.map(function (product) {
-    return renderProduct(product.title, product.price)
+    return renderProduct(product.img, product.title, product.price)
   });
   for (let i=0; i < productList.length; i++) {
     document.querySelector('.products').innerHTML += productList[i];
